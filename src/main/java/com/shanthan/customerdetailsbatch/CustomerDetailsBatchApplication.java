a5172b26de91b8f69004b2520dd742c3309c0295
Shanthan Cheruku <shanthanc@gmail.com>
2022-04-05 11:06:51 -0500

    first commit for this application

---
+++ src/main/java/com/shanthan/customerdetailsbatch/CustomerDetailsBatchApplication.java
@@ -0,0 +1,13 @@
+package com.shanthan.customerdetailsbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerDetailsBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerDetailsBatchApplication.class, args);
+    }
+
+}
